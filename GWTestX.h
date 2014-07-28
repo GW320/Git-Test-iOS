ddc03c91b02a3e13de00f3c1ea31929a2843eaff
Gösta Woll <goestawoll@Gostas-MacBook-Pro.local>
2014-07-28 15:27:51 +0200

    added a testXclass

---
+++ GWTestX.h
@@ -0,0 +1,13 @@
+//
+//  GWTestX.h
+//  Git Test
+//
+//  Created by Gösta Woll on 28.07.14.
+//  Copyright (c) 2014 Goesta Woll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GWTestX : NSObject
+
+@end
---
+++ GWTestX.m
@@ -0,0 +1,13 @@
+//
+//  GWTestX.m
+//  Git Test
+//
+//  Created by Gösta Woll on 28.07.14.
+//  Copyright (c) 2014 Goesta Woll. All rights reserved.
+//
+
+#import "GWTestX.h"
+
+@implementation GWTestX
+
+@end
